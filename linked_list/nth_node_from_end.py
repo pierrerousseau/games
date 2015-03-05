5e7f3e331e62f045fa8f3a5fec562ac1b10ceeeb
Pierre Rousseau <pierre.rousseau@openmailbox.org>
2015-03-05 06:31:16 +0100

    get the nth node from end of a linked list

---
+++ linked_list/nth_node_from_end.py
@@ -0,0 +1,45 @@
+""" Get the Kth node from end of a linked list. 
+    
+    It counts from 1 here, so the 1st node from end is the tail of list.  For 
+    instance, given a linked list with 6 nodes, whose value are 1, 2, 3, 4, 5, 
+    6, its 3rd node from end is the node with value 4.
+
+    from : http://codercareer.blogspot.fr/2011/10/no-10-k-th-node-from-end.html
+"""
+from node import List
+
+
+def find_nth_from_end_aux(nodes, index):
+    """ :returns: the kth node from the end of a linked list
+
+        :param Node nodes: the start of a list of linked nodes.
+        :param int index: the nth element from the end
+    """
+    current = (0, None)
+    if nodes is not None:
+        current_index, current_node = find_nth_from_end_aux(nodes.next, index)
+
+        if current_index == index - 1:
+            current = index + 1, str(nodes)
+        else:
+            current = current_index + 1, current_node
+
+    return current 
+
+
+def find_nth_from_end(nodes, index):
+    """ :returns: the kth node from the end of a linked list
+
+        :param Node nodes: the start of a list of linked nodes.
+        :param int index: the nth element from the end
+    """
+    nth = find_nth_from_end_aux(nodes.start, index)[1]
+    return "{} from the end of {} is {}".format(index, nodes, nth)
+
+
+if __name__ == '__main__':
+    print(find_nth_from_end(List([1, 2, 3, 4, 5, 6]), 3))
+    print(find_nth_from_end(List([1, 2, 3, 4, 5, 6]), 1))
+    print(find_nth_from_end(List([1, 2, 3, 4, 5, 6]), 6))
+    print(find_nth_from_end(List([1, 2, 3, 4, 5, 6]), 0))
+    print(find_nth_from_end(List([1, 2, 3, 4, 5, 6]), 7))
