ad73d4940cb0e8401ccb5b4a7437b6a8cd1a281f
Pierre Rousseau <pierre.rousseau@openmailbox.org>
2015-03-22 07:36:25 +0100

    quick sort

--- sort/sort.py
+++ sort/sort.py
@@ -19,3 +19,43 @@ def insertion_sort(table):
         table[jndex] = elem
 
     return table
+
+
+def partition(table, first, last):
+    """ :returns: table, pivot with pivot a element of table at its sorted 
+            place
+
+        :param list table: a list
+        :param int first: first index of the list to take into account
+        :param int last: last index of the list to take into account
+    """
+    pivot = (first + last) / 2
+
+    table[pivot], table[last] = table[last], table[pivot]
+    index = first
+    for jndex in range(first, last):
+        if table[jndex] <= table[last]:
+            table[index], table[jndex] = table[jndex], table[index]
+            index += 1
+    table[index], table[last] = table[last], table[index]
+
+    return table, index
+
+
+def quick_sort(table, first=0, last=None):
+    """ :returns: <table> sorted
+
+        Use quick sort.
+        Time complexity : o(nlogn)
+
+        :param list table: a list of values
+
+    """
+    if last is None:
+        last = len(table) - 1
+    if first < last:
+        table, pivot = partition(table, first, last)
+        table = quick_sort(table, first, pivot - 1)
+        table = quick_sort(table, pivot + 1, last)
+
+    return table
--- sort/test_sort.py
+++ sort/test_sort.py
@@ -2,7 +2,7 @@
 """
 import unittest
 
-from sort import insertion_sort
+from sort import insertion_sort, quick_sort
 
 
 class TestNode(unittest.TestCase):
@@ -34,6 +34,30 @@ class TestNode(unittest.TestCase):
         """ insertion_sort: sort reversed list
         """
         self.assertEquals(insertion_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+
+
+    def test_quick_sort_1(self):
+        """ quick_sort: sort a lsit
+        """
+        self.assertEquals(quick_sort([5, 1, 2, 4, 3]), [1, 2, 3, 4, 5])
+
+
+    def test_quick_sort_2(self):
+        """ quick_sort: sort an empty list
+        """
+        self.assertEquals(quick_sort([]), [])
+
+
+    def test_quick_sort_3(self):
+        """ quick_sort: sort sorted list
+        """
+        self.assertEquals(quick_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+
+    def test_quick_sort_4(self):
+        """ quick_sort: sort reversed list
+        """
+        self.assertEquals(quick_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
 
 
 if __name__ == '__main__':
