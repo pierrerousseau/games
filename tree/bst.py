731bcbe3590b78ff9fbdf63cb81206befdfb26c8
Pierre Rousseau <pierre.rousseau@openmailbox.org>
2015-03-22 06:45:36 +0100

    remove a node from a bst

--- tree/bst.py
+++ tree/bst.py
@@ -1,6 +1,6 @@
 """ Basic binary search tree data structure and functions.
 """
-from binary_tree import Node
+from binary_tree import Node, nodes_to_str
 
 def add_to_nodes(nodes, node):
     """ Add a node to a bst.
@@ -37,5 +37,40 @@ def create_nodes(data=None):
             raise TypeError
         for elem in data:
             nodes = add_to_nodes(nodes, Node(elem))
+
+    return nodes
+
+
+def find_max(nodes):
+    """ :returns: the node with the maximum value of the bst
+
+        :param Node nodes: the root node of a bst
+    """
+    max_node = nodes
+    if max_node is not None and max_node.right is not None:
+        max_node = find_max(nodes.right)
+
+    return max_node
+
+
+def remove(value, nodes):
+    """ :returns: <nodes> without <node>
+
+        :param int value: the data of the node to remove
+        :parma Node nodes: the root node of a bst
+    """
+    if nodes is not None:
+        if value > nodes.data:
+            nodes.right = remove(value, nodes.right)
+        elif value < nodes.data:
+            nodes.left = remove(value, nodes.left)
+        else:
+            if nodes.left:
+                max_smaller       = find_max(nodes.left)
+                max_smaller.left  = remove(max_smaller.data, nodes.left)
+                max_smaller.right = nodes.right
+                nodes = max_smaller
+            else:
+                nodes = nodes.right
 
     return nodes
--- tree/test_bst.py
+++ tree/test_bst.py
@@ -4,7 +4,7 @@ import unittest
 
 from binary_tree import Node, nodes_to_str, in_traversal, post_traversal, \
     pre_traversal
-from bst import create_nodes
+from bst import create_nodes, find_max, remove
 
 
 class TestNode(unittest.TestCase):
@@ -49,6 +49,77 @@ class TestNode(unittest.TestCase):
         result = [1, 3, 2, 5, 7, 6, 4]
 
         self.assertEquals(post_traversal(tree), result)
+
+
+    def test_find_max_1(self):
+        """ find_max: find the max node of a bst
+        """
+        tree   = create_nodes([])
+        result = None
+
+        self.assertEquals(str(find_max(tree)), str(result))
+
+
+    def test_find_max_2(self):
+        """ find_max: find the max node of a bst
+        """
+        tree   = create_nodes([4, 2, 6, 3, 1, 5, 7])
+        result = " 7 "
+
+        self.assertEquals(str(find_max(tree)), result)
+
+
+    def test_find_max_3(self):
+        """ find_max: find the max node of a bst
+        """
+        tree   = create_nodes([4, 2, 6, 3, 1, 5])
+        result = " 6 "
+
+        self.assertEquals(str(find_max(tree)), result)
+
+
+    def test_remove_1(self):
+        """ remove: remove a node from a bst
+        """
+        tree   = create_nodes([4, 2, 6, 3, 1, 5, 7])
+        clean  = remove(7, tree)
+
+        self.assertEquals(in_traversal(clean), [1, 2, 3, 4, 5, 6])
+
+    def test_remove_2(self):
+        """ remove: remove a leaf from a bst
+        """
+        tree   = create_nodes([4, 2, 6, 3, 1, 5, 7])
+        clean  = remove(8, tree)
+
+        self.assertEquals(in_traversal(clean), [1, 2, 3, 4, 5, 6, 7])
+
+
+    def test_remove_3(self):
+        """ remove: remove a node from an empty bst
+        """
+        tree   = create_nodes([])
+        clean  = remove(7, tree)
+
+        self.assertEquals(in_traversal(clean), [])
+
+
+    def test_remove_4(self):
+        """ remove: remove root node from a bst
+        """
+        tree   = create_nodes([4, 2, 6, 3, 1, 5, 7])
+        clean  = remove(4, tree)
+
+        self.assertEquals(in_traversal(clean), [1, 2, 3, 5, 6, 7])
+
+
+    def test_remove_5(self):
+        """ remove: remove a middle node from a bst
+        """
+        tree   = create_nodes([4, 2, 6, 3, 1, 5, 7])
+        clean  = remove(5, tree)
+
+        self.assertEquals(in_traversal(clean), [1, 2, 3, 4, 6, 7])
 
 
 if __name__ == '__main__':
